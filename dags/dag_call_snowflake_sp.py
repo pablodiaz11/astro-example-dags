863cba2dcfd3eea49cf3ed67b26ac8b44a0cdca4
Pablo Diaz <pablo.diaz@moelis.com>
2024-05-09 15:27:04 -0500

    astro-runtime:11.3.0

--- dags/dag_call_snowflake_sp.py
+++ dags/dag_call_snowflake_sp.py
@@ -33,14 +33,12 @@ with DAG(
     #SQL_CALL_SP = f"call {SNOWFLAKE_SP}('RUN_DATE' TIMESTAMP_TZ(9))"
     #SQL_CALL_SP = f"call {SNOWFLAKE_SP}(?, ?, ?)"
     SQL_CALL_SP = f"call {SNOWFLAKE_SP}(%(feed_date)s, %(process_name)s, %(status)s)"
-    #SQL_CALL_SP = f"call {SNOWFLAKE_SP}('2023-09-27', 'S&P API', 'Failed')"
 
     populate_interaction_company = SnowflakeOperator(
         task_id = "populate_interaction_company",
         sql = SQL_CALL_SP,
         autocommit = True,
         parameters = params,
-        #parameters= {"feed_date":"2023-09-27","process_name":"S&P API","status":"Failed"}, 
         #parameters = [params['feed_date'], params['process_name'],params['status']],
     )
 
