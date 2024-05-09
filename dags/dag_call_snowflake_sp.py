163f1c461c0a252ae58e56b5a5a78740f1092164
Pablo Diaz <pablo.diaz@moelis.com>
2024-05-09 14:36:54 -0500

    added transaction and commit

--- dags/dag_call_snowflake_sp.py
+++ dags/dag_call_snowflake_sp.py
@@ -11,7 +11,7 @@ from airflow.operators.python import PythonOperator
 DAG_ID = "dag_call_snowflake_sp"
 SNOWFLAKE_CONN_ID = "snow_devtest"
 #SNOWFLAKE_SP = "ODS.META_DATA.POPULATE_INTERACTION_COMPANY"
-SNOWFLAKE_SP = "STAGE.SP_PROCESS_RUN_END_v2"
+SNOWFLAKE_SP = "STAGE.SP_PROCESS_RUN_END_v3"
 
 with DAG(
     DAG_ID,
