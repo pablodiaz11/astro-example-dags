366d5aa5828590e73ca476c042e9c5d833680164
Pablo Diaz <pablo.diaz@moelis.com>
2024-05-10 19:55:20 -0500

    fix cnx issue

--- dags/snp/dag_import_snp_entities.py
+++ dags/snp/dag_import_snp_entities.py
@@ -9,6 +9,8 @@ from airflow.operators.bash import BashOperator
 from airflow.models import Variable
 import pendulum
 import pytz
+import snowflake.connector
+from snowflake.connector.pandas_tools import write_pandas
 
 local_tz = pendulum.timezone("America/New_York")
 dag_id = "dag_import_snp_entities"
@@ -40,12 +42,42 @@ with DAG(
     var_fs_username = Variable.get('var_fs_username', default_var = None)
     var_fs_apikey = Variable.get('var_fs_apikey', default_var = None)
     var_fs_url = Variable.get('var_fs_url', default_var = None)
+    # Snowflake
+    var_snow_user = Variable.get('var_snow_user', default_var = None)
+    var_snow_password = Variable.get('var_snow_password', default_var = None)
+    var_snow_account = Variable.get('var_snow_account', default_var = None)
+    var_snow_role = Variable.get('var_snow_role', default_var = None)
+    var_snow_warehouse = Variable.get('var_snow_warehouse', default_var = None)
+    var_snow_database = Variable.get('var_snow_database', default_var = None)
+    var_snow_schema_stg = Variable.get('var_snow_schema_stg', default_var = None)
+    var_snow_schema_gold = Variable.get('var_snow_schema_gold', default_var = None)
+
+    conn_stg = snowflake.connector.connect(  
+        user = var_snow_user,
+        password = var_snow_password,
+        account = var_snow_account,
+        role = var_snow_role,
+        warehouse = var_snow_warehouse,
+        database = var_snow_database,
+        schema = var_snow_schema_stg
+    )
+
+    conn_gold = snowflake.connector.connect(  
+        user = var_snow_user,
+        password = var_snow_password,
+        account = var_snow_account,
+        role = var_snow_role,
+        warehouse = var_snow_warehouse,
+        database = var_snow_database,
+        schema = var_snow_schema_gold
+    )
 
     op_kwargs = {
         'p_snp_username': var_snp_username,
         'p_snp_password': var_snp_password,
         'p_snp_url': var_snp_url,
-        'p_cnx_snow_dsa_stage': cnx_snow_dsa_stage
+        'p_conn_stg': conn_stg,
+        'p_conn_gold': conn_gold
     }
 
     def call_procedure_list(procedure_query, conn):
@@ -55,11 +87,12 @@ with DAG(
         result = cur.fetchall()
         return result
 
-    def snp_import_entities(p_snp_username, p_snp_password, p_snp_url, p_cnx_snow_dsa_stage):
+    def snp_import_entities(p_snp_username, p_snp_password, p_snp_url, p_conn_stg, p_conn_gold):
         snp_username = p_snp_username
         snp_password = p_snp_password
         snp_url = p_snp_url
-        conn_stg = p_cnx_snow_dsa_stage
+        conn_stg = p_conn_stg
+        conn_gold = p_conn_gold
 
         try:
             #---------------------------------------
