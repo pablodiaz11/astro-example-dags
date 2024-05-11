60905a160f5fa2ca73822477cf9ca21a4f03c480
Pablo Diaz <pablo.diaz@moelis.com>
2024-05-10 19:29:25 -0500

    added start process

--- dags/snp/dag_import_snp_entities.py
+++ dags/snp/dag_import_snp_entities.py
@@ -30,9 +30,7 @@ with DAG(
     schedule = None,
     catchup = False,
 ) as dag:
-    # Star process
-    begin = EmptyOperator(task_id="begin")
-
+    
     # SnP API
     var_snp_username = Variable.get('var_snp_username', default_var = None)
     var_snp_password = Variable.get('var_snp_password', default_var = None)
@@ -45,22 +43,59 @@ with DAG(
     op_kwargs = {
         'p_snp_username': var_snp_username,
         'p_snp_password': var_snp_password,
-        'p_snp_url': var_snp_url
+        'p_snp_url': var_snp_url,
+        'p_cnx_snow_dsa_stage': cnx_snow_dsa_stage
     }
 
-    def snp_import_entities(p_snp_username, p_snp_password, p_snp_url):
+    def call_procedure_list(procedure_query, conn):
+        query = f'{procedure_query};'
+        cur = conn.cursor()
+        cur.execute(query)
+        result = cur.fetchall()
+        return result
+
+    def snp_import_entities(p_snp_username, p_snp_password, p_snp_url, p_cnx_snow_dsa_stage):
         snp_username = p_snp_username
         snp_password = p_snp_password
         snp_url = p_snp_url
-        message = f"value from variable: {snp_username} | {snp_password} | {snp_url}!"
-        print(message)
+        conn_stg = p_cnx_snow_dsa_stage
 
+        try:
+            #---------------------------------------
+            #:::::::::::::: Start Process ::::::::::
+            #---------------------------------------
+
+            start_date = '2024-05-06'
+            end_date = datetime.now(tz=pytz.timezone('US/Eastern')).strftime('%Y-%m-%d') #'2023-02-17'
+            process_name = 'S&P API'
+            status = 'Pending'
+
+            sp_query = f"call STAGE.SP_PROCESS_RUN_START('{start_date}','{end_date}','{process_name}','{status}');"
+            business_day = call_procedure_list(sp_query,conn_stg)
+
+            print(f'==> Process ended successful') 
+
+        except Exception as e:
+            print('\n')
+            print("Exception Name: {}".format(type(e).__name__))
+            print("Exception Description: {}".format(e))
+
+            sp_query_end = f"call STAGE.SP_PROCESS_RUN_END('{bdate}','{process_name}','Failed');"
+            r = call_procedure_list(sp_query_end,conn_stg)
+            print(f'==> Process ended: {r[0][0]}') 
+            print('')
+
+    # Start process
+    begin = EmptyOperator(task_id="begin")
+
+    # Import entities
     import_snp_entities = PythonOperator(
         task_id = "import_snp_entities",
         python_callable = snp_import_entities,
         op_kwargs = op_kwargs,
     )
-
+    
+    # End process
     end = EmptyOperator(task_id="end")
 
 begin >> import_snp_entities >> end
